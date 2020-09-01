f95e3363f3e5b7b3084c7205ba973b86e2678de6
Nic Barnes <colorpulse@gmail.com>
2020-09-01 17:21:02 +0200

    lading gif

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { RoomContext, RoomConsumer } from '../context'
-
+import Loading from './Loading'
+import Room from '../components/Room'
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-       
+       const { featuredRooms : rooms } = this.context
+       console.log(rooms)
         return (
             <div>
                Featured Rooms 
+                <Room />
+               <Loading />
             </div>
         )
     }
